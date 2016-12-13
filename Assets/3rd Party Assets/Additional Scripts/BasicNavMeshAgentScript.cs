40d048a3c6c7e24782c493448ee0da376a2672e1
D-Hin <dominic.hin@my.jcu.edu.au>
2016-12-13 14:29:05 +1000

    Environment & Scripting
    Began creating additional/modified script(s) to be used within the
    MainScene, along Baking a NavMesh into the floors – with NavMesh
    Obstacle components being included in the Walls.

---
+++ Assets/3rd Party Assets/Additional Scripts/BasicNavMeshAgentScript.cs
@@ -0,0 +1,194 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicNavMeshAgentScript : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update () {
+		agent.SetDestination (target.position);
+	}
+}
+
+/* Note: The following is based off of 2 seperate CS files made during another Project (IT@JCU, CP3311, 2016)
+ * 
+ * TODO: ONLY TAKE OUT CODE SEGMENTS RELEVANT FOR PROJECT! DELETE REST WHEN DONE!!
+ * 
+ */
+
+//using UnityEngine;
+//
+//public class EnemyHealth : MonoBehaviour
+//{
+//    public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+//    public int currentHealth;                   // The current health the enemy has.
+//    public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
+//    public int scoreValue = 10;                 // The amount added to the player's score when the enemy dies.
+//    //public AudioClip deathClip;                 // The sound to play when the enemy dies.
+//
+//    Animator anim;                              // Reference to the animator.
+//    //AudioSource enemyAudio;                     // Reference to the audio source.
+//    //ParticleSystem hitParticles;                // Reference to the particle system that plays when the enemy is damaged.
+//    CapsuleCollider capsuleCollider;            // Reference to the capsule collider.
+//    bool isDead;                                // Whether the enemy is dead.
+//    bool isSinking;                             // Whether the enemy has started sinking through the floor.
+//
+//    void Awake () {
+//        // Setting up the references.
+//        anim = GetComponent <Animator> ();
+//        //enemyAudio = GetComponent <AudioSource> ();
+//        //hitParticles = GetComponentInChildren <ParticleSystem> ();
+//        capsuleCollider = GetComponent <CapsuleCollider> ();
+//
+//        // Setting the current health when the enemy first spawns.
+//        currentHealth = startingHealth;
+//    }
+//
+//
+//    void Update () {
+//        // check if enemy is dead
+//		if(isDead)
+//        {
+//            // ... move the enemy down by the sinkSpeed per second.
+//            //transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+//			Destroy (gameObject, 2f);
+//        }
+//    }
+//
+//	void OnTriggerEnter(Collider other) {
+//		//check if collider is the players punch hit box
+//		PlayerHealth health = other.gameObject.GetComponentInParent<PlayerHealth>();
+//		if (health != null) {
+//			//take damge from the hit
+//			TakeDamage (50);
+//			//debug checking hitbox
+//			print ("hit");
+//		} else {
+//			print ("miss");
+//		}
+//	}
+//
+//    public void TakeDamage (int amount) {
+//        // If the enemy is dead...
+//        if(isDead)
+//            // ... no need to take damage so exit the function.
+//            return;
+//
+//        // Play the hurt sound effect.
+//        //enemyAudio.Play ();
+//
+//        // Reduce the current health by the amount of damage sustained.
+//        currentHealth -= amount;
+//        
+//        // Set the position of the particle system to where the hit was sustained.
+//        //hitParticles.transform.position = hitPoint;
+//
+//        // And play the particles.
+//        //hitParticles.Play();
+//
+//        // If the current health is less than or equal to zero...
+//		if (currentHealth <= 0) {
+//			// ... the enemy is dead.
+//			Death ();
+//		} else {
+//			//just recoil from the blow, not die
+//			anim.SetTrigger("Recoil");
+//		}
+//    }
+//
+//    void Death ()
+//    {
+//        // The enemy is dead.
+//        isDead = true;
+//
+//        // Turn the collider into a trigger so shots can pass through it.
+//        capsuleCollider.isTrigger = true;
+//
+//        // Tell the animator that the enemy is dead.
+//        anim.SetTrigger ("Dead");
+//
+//        // Change the audio clip of the audio source to the death clip and play it (this will stop the hurt clip playing).
+//        //enemyAudio.clip = deathClip;
+//        //enemyAudio.Play ();
+//    }
+//
+//    public void StartSinking () {
+//        // Find and disable the Nav Mesh Agent.
+//        GetComponent <NavMeshAgent> ().enabled = false;
+//
+//        // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+//        GetComponent <Rigidbody> ().isKinematic = true;
+//
+//        // The enemy should no sink.
+//        isSinking = true;
+//
+//        // Increase the score by the enemy's score value.
+//        //ScoreManager.score += scoreValue;
+//
+//        // After 2 seconds destory the enemy.
+//        Destroy (gameObject, 2f);
+//    }
+//}
+
+
+
+//using UnityEngine;
+//using System.Collections;
+//
+//public class EnemyMovement : MonoBehaviour
+//{
+//	public float detectDistance = 15f;
+//	public float attackRange = 1.5f;
+//	float distance;					
+//    Transform player;               // Reference to the player's position.
+//    PlayerHealth playerHealth;      // Reference to the player's health.
+//	Transform enemy;				// Reference to this enemy's position.
+//    EnemyHealth enemyHealth;        // Reference to this enemy's health.
+//	Animator anim;					
+//    NavMeshAgent nav;
+//
+//    void Awake ()
+//    {
+//        // Set up the references.
+//        player = GameObject.FindGameObjectWithTag ("Player").transform;
+//		enemy = GetComponent <Transform> ();
+//        playerHealth = player.GetComponent <PlayerHealth> ();
+//        enemyHealth = GetComponent <EnemyHealth> ();
+//        nav = GetComponent <NavMeshAgent> ();
+//		anim = GetComponent <Animator> ();
+//    }
+//
+//    void Update ()
+//	{	
+//		distance = Vector3.Distance (enemy.position, player.position);			// checks how far away the player is
+//		//if player is within range enemy detects them
+//		if (distance <= detectDistance && distance > attackRange) {
+//			anim.SetTrigger ("PlayerDetected");
+//			//if player is in attack range
+//
+//			// If the enemy and the player have health left...
+//			if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) {
+//				// ... set the destination of the nav mesh agent to the player.
+//				nav.SetDestination (player.position);
+//				transform.LookAt (player.position);
+//			}
+//            // Otherwise...
+//            else {
+//					// ... disable the nav mesh agent.
+//					nav.enabled = false;
+//				}
+//		}
+//		//if enemy in attack range
+//		if (distance <= attackRange) {
+//			anim.SetTrigger ("PlayerInAttackRange");
+//			if (anim.GetCurrentAnimatorStateInfo(0).IsName("attack") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.6f) {
+//				playerHealth.TakeDamage (10);
+//			}
+//		}
+//	}
+//}
---
+++ Assets/3rd Party Assets/Additional Scripts/SlidingDoorScript.cs
@@ -0,0 +1,207 @@
+﻿/* Code derived from Alexander Ameye's DOOR SCRIPT file: "Door.cs" (v1.0.3 - 10/19/2016) */
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class SlidingDoorScript : MonoBehaviour {
+
+	// INSPECTOR SETTINGS
+	[Header("Sliding Door Settings")]
+	[Tooltip("The intial position of the door/window. (Relative to current location of self in Scene)")]
+	public float InitialPosition = 0.0f;
+	[Tooltip("The number of [undefined] units the door/window translates.")]
+	public float TranslatedPosition = 90.0f;
+
+	public enum PositionOfHinge {
+		Left,
+		Right,
+	}
+	public PositionOfHinge HingePosition;
+
+	public enum SideOfTranslation {
+		Left,
+		Right,
+	}
+	public SideOfTranslation TranslationSide;
+
+	[Tooltip("Translation speed of the door/window.")]
+	public float TranslationSpeed = 3f;
+	[Tooltip("0 = infintie times")]
+	public int TimesMoveable = 0;
+
+	// PRIVATE SETTINGS
+	private int n = 0; // for "TimesMoveable" loop
+	[HideInInspector] public bool Running = false;
+
+	// DEBUGGING
+	[Header("Debug Settings")]
+	[Tooltip("Visualises the position of the hinge in-game by a coloured cube.")]
+	public bool VisualiseHinge = false;
+	[Tooltip("The visualisation colour of the hinge.")]
+	public Color HingeColour = Color.cyan;
+
+//	// Defines intial and final translation points
+
+//Define an initial and final rotation.
+//	private Quaternion FinalRot, InitialRot;
+//	private int State;
+
+	// Creates a hinge
+	GameObject hinge;
+
+	// START FUNCTION (used for initialization)
+	void Start () {
+		// Gives object the name "Door" for future reference
+		gameObject.tag = "Door";
+
+		// Creates hinge
+		hinge = new GameObject();
+		hinge.name = "hinge";
+
+		//Calculate sine/cosine of initial angle (needed for hinge positioning).
+		float CosDeg = Mathf.Cos ((transform.eulerAngles.y * Mathf.PI) / 180);
+		float SinDeg = Mathf.Sin ((transform.eulerAngles.y * Mathf.PI) / 180);
+
+		// Reads transform (position/rotation/scale) of the door
+		float DoorPositX = transform.position.x;
+		float DoorPositY = transform.position.y;
+		float DoorPositZ = transform.position.z;
+
+		float DoorRotateX = transform.position.x;
+//		float DoorRotateY = transform.position.y;
+		float DoorRotateZ = transform.position.z;
+
+		float DoorScaleX = transform.localScale.x;
+//		float DoorScaleY = transform.localScale.y;
+		float DoorScaleZ = transform.localScale.z;
+
+		// Creates a placeholder of the hinge's position/rotation
+		Vector3 HingePositCopy = hinge.transform.position;
+		Vector3 HingeRotateCopy = hinge.transform.localEulerAngles;
+
+		// HINGE LEFT
+		if (HingePosition == PositionOfHinge.Left)
+		{
+			// CALCULATE
+			if (transform.localScale.x > transform.localScale.z)
+			{
+				HingePositCopy.x = (DoorPositX - (DoorScaleX / 2 * CosDeg));
+				HingePositCopy.z = (DoorPositZ + (DoorScaleX / 2 * SinDeg));
+				HingePositCopy.y = DoorPositY;
+		//*TODO*
+				HingeRotateCopy.x = DoorRotateX;
+				HingeRotateCopy.y = -InitialPosition;
+				HingeRotateCopy.z = DoorRotateZ;
+			} else {
+				HingePositCopy.x = (DoorPositX + (DoorScaleZ / 2 * SinDeg));
+				HingePositCopy.z = (DoorPositZ + (DoorScaleZ / 2 * CosDeg));
+				HingePositCopy.y = DoorPositY;
+		//*TODO*
+				HingeRotateCopy.x = DoorRotateX;
+				HingeRotateCopy.y = -InitialPosition;
+				HingeRotateCopy.z = DoorRotateZ;
+			}
+		}
+
+		// HINGE RIGHT
+		if (HingePosition == PositionOfHinge.Right)
+		{
+			// CALCULATE
+			if (transform.localScale.x > transform.localScale.z)
+			{
+				HingePositCopy.x = (DoorPositX + (DoorScaleX / 2 * CosDeg));
+				HingePositCopy.z = (DoorPositZ - (DoorScaleX / 2 * SinDeg));
+				HingePositCopy.y = DoorPositY;
+		//*TODO*
+				HingeRotateCopy.x = DoorRotateX;
+				HingeRotateCopy.y = -InitialPosition;
+				HingeRotateCopy.z = DoorRotateZ;
+			} else {
+				HingePositCopy.x = (DoorPositX - (DoorScaleZ / 2 * SinDeg));
+				HingePositCopy.z = (DoorPositZ - (DoorScaleZ / 2 * CosDeg));
+				HingePositCopy.y = DoorPositY;
+		//*TODO*
+				HingeRotateCopy.x = DoorRotateX;
+				HingeRotateCopy.y = -InitialPosition;
+				HingeRotateCopy.z = DoorRotateZ;
+			}
+		}
+
+		// HINGE POSITIONING
+		hinge.transform.position = HingePositCopy;
+		transform.parent = hinge.transform;
+		hinge.transform.localEulerAngles = HingeRotateCopy;
+
+		// DEBUGGING
+		if (VisualiseHinge == true) {
+			GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			cube.transform.position = HingePositCopy;
+			cube.transform.localScale = new Vector3 (0.25f, 0.5f, 0.25f);
+			cube.GetComponent<Renderer> ().material.color = HingeColour;
+		}
+
+		// ERROR CODE	* Has TODO with RotationAngles *
+		if (Mathf.Abs (InitialPosition) + Mathf.Abs (TranslatedPosition) == 180 
+			|| Mathf.Abs (InitialPosition) + Mathf.Abs (TranslatedPosition) > 180) 
+		{
+			UnityEditor.EditorUtility.DisplayDialog ("Error 001", "The difference between 'Initial Position' " 
+				+ "and 'Translated Position' cannot exceed or be equal to 180 degrees.", "OK", "");
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+
+//		// ANGLES
+//		if (RotationSide == SideOfRotation.Left)
+//		{
+//			InitialRot = Quaternion.Euler (0, -InitialAngle, 0);
+//			FinalRot = Quaternion.Euler(0, -InitialAngle - RotationAngle, 0);
+//		}
+//
+//		if (RotationSide == SideOfRotation.Right)
+//		{
+//			InitialRot = Quaternion.Euler (0, -InitialAngle, 0);
+//			FinalRot = Quaternion.Euler(0, -InitialAngle + RotationAngle, 0);
+//		}
+
+	}
+
+	// UPDATE FUNCTION (called once per frame)
+	void Update () {}
+
+//	// OPEN FUNCTION
+//	public IEnumerator Open ()
+//    {
+//		if (n < TimesMoveable || TimesMoveable == 0)
+//		{
+//			//Change state from 1 to 0 and back (= alternate between FinalRot and InitialRot).
+//			if (hinge.transform.rotation == (State == 0 ? FinalRot : InitialRot)) State ^= 1;
+//
+//			//Set 'FinalRotation' to 'FinalRot' when moving and to 'InitialRot' when moving back.
+//			Quaternion FinalRotation = ((State == 0) ? FinalRot : InitialRot);
+//
+//			//Make the door/window rotate until it is fully opened/closed.
+//			while (Mathf.Abs(Quaternion.Angle(FinalRotation, hinge.transform.rotation)) > 0.01f)
+//			{
+//				Running = true;
+//				hinge.transform.rotation = Quaternion.Lerp (hinge.transform.rotation, FinalRotation, Time.deltaTime * Speed);
+//				yield return new WaitForEndOfFrame();
+//			}
+//			Running = false;
+//
+//			if (TimesMoveable == 0) n = 0;
+//			else n++;
+//		}
+//	}
+
+	// GUI FUNCTION
+	void OnGUI () {
+		// Accesses the 'InReach' variable from raycasting script
+		GameObject Player = GameObject.Find("Player");
+		Detection detection = Player.GetComponent<Detection> ();
+
+		if (detection.InReach == true) {
+			GUI.color = Color.white;
+			GUI.Box (new Rect (20, 20, 200, 25), "Press 'E' to Open/Close the Door");
+		}
+	}
+}
